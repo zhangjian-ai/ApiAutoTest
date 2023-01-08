b46247346bcee2fc79981aba54ac89e7e006c1e6
zhangjian1138 <zhangjian1138@wezhuiyi.com>
2023-01-08 18:23:25 +0800

    增加 in、not in、any 三种包含关系校验逻辑

--- utils/common/execute.py
+++ utils/common/execute.py
@@ -1,9 +1,18 @@
+"""
+@Project: bot3-env-verify
+@File: execute.py
+@Author: Seeker
+@Date: 2022/10/26 7:16 下午
+"""
+import os
 import json
 
 import requests
 import traceback
 
-from utils.common import log
+from urllib import parse
+
+from utils.common import log, TEMP_DIR
 
 
 def http_request(method="POST", url=None, data=None, params=None, headers=None, cookies=None, detail=True):
@@ -97,48 +106,49 @@ def verify(expect: dict, response: dict):
 
         # 包含关系
         elif key in ("in", "not in"):
-            real_value = response
-            if not isinstance(real_value, (list, str, dict)):
+            if not isinstance(response, (list, str, dict)):
                 assert False, f'\n' \
                               f'KEY: {key} \n' \
                               f'运算符: {key} ' \
                               f'预期值: {value}\n' \
-                              f'实际值: {real_value} 的类型不支持包含关系运算\n'
-            elif isinstance(value, dict):
-                status = False
-                for item in response:
-                    if not isinstance(item, dict):
-                        assert False, f'\n' \
-                                      f'KEY: {key} \n' \
-                                      f'运算符: {key} ' \
-                                      f'预期值: {value}\n' \
-                                      f'实际值: {real_value} 预期值无法与迭代项做比较\n'
-                    try:
-                        verify(value, item)
-                    except:
-                        pass
-                    else:
-                        status = True
-                        break
+                              f'实际值: {response} 的类型不支持包含关系运算\n'
 
-                if not status:
-                    assert False, f'\n' \
-                                  f'KEY: {key} \n' \
-                                  f'运算符: {key} ' \
-                                  f'预期值: {value}\n' \
-                                  f'实际值: {real_value}\n' \
-                                  f'返回值对象中没有与预期值匹配的项'
+            elif isinstance(value, dict):
+                _verify_list(key, [value], response, operate=key)
 
             elif isinstance(value, (list, tuple)):
-                _verify_list(key, value, expect, {key: response})
+                _verify_list(key, value, response, operate=key)
 
             else:
-                assert eval(f"{value} {key} {real_value}"), f'\n' \
-                                                            f'KEY: {key} \n' \
-                                                            f'运算符: {key} ' \
-                                                            f'预期值: {value}\n' \
-                                                            f'实际值: {real_value}\n' \
-                                                            f'返回值对象中没有与预期值匹配的项'
+                assert eval(f"{value} {key} {response}"), f'\n' \
+                                                          f'KEY: {key} \n' \
+                                                          f'运算符: {key} ' \
+                                                          f'预期值: {value}\n' \
+                                                          f'实际值: {response}\n' \
+                                                          f'包含关系运算结果不为真'
+
+        # any 预期列表中任意一个值在实际响应中就算通过
+        elif key in ("any",):
+            # 使用 any 时，要求预期值是列表或元组
+            assert isinstance(value, (list, tuple)), f'\n' \
+                                                     f'KEY: {key} \n' \
+                                                     f'预期值: {value} \n' \
+                                                     f'使用 any 关键字时，要求预期值是列表或元组'
+            success = False
+            # 将预期值依次放入列表校验，直到校验成功
+            for sub_val in value:
+                try:
+                    _verify_list(key, [sub_val], response)
+                    success = True
+                    break
+                except AssertionError:
+                    pass
+
+            assert success, f'\n' \
+                            f'KEY: {key} \n' \
+                            f'预期值: {value} \n' \
+                            f'预期值: {response} \n' \
+                            f'预期结果中没有任何值在实际列表中存在'
 
         # 如果预期值是str、int、float那么就直接对比
         elif isinstance(value, (str, int, float)):
@@ -150,7 +160,7 @@ def verify(expect: dict, response: dict):
 
         # 如果预期值是list则便利预期列表中的item是否都在实际结果中能找到
         elif isinstance(value, (list, tuple)):
-            _verify_list(key, value, expect, response)
+            _verify_list(key, value, response.get(key))
 
         # 如果预期值是dict则再次调用本方法进行递归验证
         elif isinstance(value, dict):
@@ -161,22 +171,22 @@ def verify(expect: dict, response: dict):
             verify(value, response.get(key))
 
 
-def _verify_list(key, value, expect, response):
+def _verify_list(key: str, expect: list, response: list, operate: str = "in"):
     """
     针对列表验证
+    key 为预期列表expect在父级字典对应的键，传递过来主要为了打印日志
+    operate 取值为 in、not in 用于表示包含和不包含的关系
     """
-    assert isinstance(response, dict), f'\n' \
+    assert isinstance(response, list), f'\n' \
                                        f'预期结果: {json.dumps(expect, indent=2, ensure_ascii=False)}\n' \
                                        f'实际结果: {json.dumps(response, indent=2, ensure_ascii=False)}\n' \
-                                       f"响应对象实际类型是: {type(response)}，预期类型是 DICT\n"
-
-    real_value = response.get(key)
+                                       f"响应对象实际类型是: {type(response)}，预期类型是 LIST\n"
 
     # 检查实际值的类型
-    assert isinstance(real_value, (list, tuple)), f'\n' \
-                                                  f'KEY: {key} \n' \
-                                                  f'预期值: {value}\n' \
-                                                  f'实际值: {real_value}\n'
+    assert isinstance(response, (list, tuple)), f'\n' \
+                                                f'KEY: {key} \n' \
+                                                f'预期值: {expect}\n' \
+                                                f'实际值: {response}\n'
 
     # # 列表长度校验
     # if len(value) != len(real_value):
@@ -186,32 +196,38 @@ def _verify_list(key, value, expect, response):
     #                   f'实际列表长度: {len(real_value)}\n'
 
     # 列表项依次校验
-    for index, sub_value in enumerate(value):
+    for sub_val in expect:
         # 此处不考虑列表项仍然是列表的情况，此种情况也不能出现在脚本中
-        # 常规类型时，考虑返回值可能无序，因此这里用 in 断言
-        if isinstance(sub_value, (str, int, float)):
-            assert sub_value in real_value, f'\n' \
-                                            f'KEY: {key}\n' \
-                                            f'预期列表: {value}\n' \
-                                            f'实际列表: {real_value}\n' \
-                                            f'缺失表项: {sub_value}'
-
-        # 如果列表项是字典，那么要求需要校验的列表项要和实际响应列表项按索引对应
-        elif isinstance(sub_value, dict):
-            # 判断索引是否合法
-            if index >= len(real_value):
-                assert False, f'\n' \
-                              f'KEY: {key}\n' \
-                              f'预期列表: {value}\n' \
-                              f'实际列表: {real_value}\n' \
-                              f'预期索引值({index})无法在实际响应结果中使用'
-
-            # 判断返回值列表该索引处是否也为字典，如果是则 递归调用 本方法
-            if isinstance(real_value[index], dict):
-                verify(sub_value, real_value[index])
-                continue
+        if isinstance(sub_val, (list, tuple)):
             assert False, f'\n' \
                           f'KEY: {key}\n' \
-                          f'索引值: {index}\n' \
-                          f'预期列表该索引处的值: {sub_value}\n' \
-                          f'实际列表该索引处的值: {real_value[index]}'
+                          f'预期值: {expect}\n' \
+                          f'结果验证中不允许列表嵌套列表的预期值'
+
+        # 常规类型时，考虑返回值可能无序，直接用 operate 执行运算
+        elif isinstance(sub_val, (str, int, float)):
+            assert eval(f"{sub_val} {operate} {response}"), f'\n' \
+                                                            f'KEY: {key}\n' \
+                                                            f'预期列表: {expect}\n' \
+                                                            f'实际列表: {response}\n' \
+                                                            f'缺失表项: {sub_val}'
+
+        # 如果列表项是字典，那么就便利实际结果列表依次对比直到通过
+        elif isinstance(sub_val, dict):
+            status = operate == "not in"
+            for item in response:
+                if not isinstance(item, dict):
+                    continue
+                try:
+                    verify(sub_val, item)
+                    status = not status
+                    break
+                except AssertionError:
+                    pass
+
+            assert status, f'\n' \
+                           f'KEY: {key} \n' \
+                           f'运算符: {operate} \n' \
+                           f'预期值: {sub_val}\n' \
+                           f'实际值: {response}\n' \
+                           f'包含关系运算结果不为真'
